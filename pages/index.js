4977b4f02dd1ee81e22ebf91f1aeeeef6e465c1b
ve-verdian <masukankatasandi2292@gmail.com>
2023-01-04 11:43:07 +0700

    Perubahan Link

--- pages/index.js
+++ pages/index.js
@@ -22,25 +22,25 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="http://175.106.8.138:9099" target="_blank" rel="noopener noreferrer" className={styles.card}>
+          {/* <a href="http://175.106.8.138:9099" target="_blank" rel="noopener noreferrer" className={styles.card}>
             <h2>Teramedik &rarr;</h2>
             <p>Hospital Information System, Rumah Sakit Ibu & Anak Family</p>
-          </a>
+          </a> */}
 
           <a href="http://175.106.8.138:9393/" target="_blank" rel="noopener noreferrer" className={styles.card}>
             <h2>RSCLOUD &rarr;</h2>
             <p>Rumah yang aman untuk semua data Anda, alangkah baiknya di simpan di Cloud</p>
           </a>
 
-          <a href="http://175.106.8.138:9292/" target="_blank" rel="noopener noreferrer" className={styles.card}>
-            <h2>Sismadak &rarr;</h2>
+          <a href="http://akreditasi.rsiafamily.com/login.php" target="_blank" rel="noopener noreferrer" className={styles.card}>
+            <h2>SIDOKARS v1.2 &rarr;</h2>
             <p>Mengumpulkan, menyimpan, dan mencari dokumen Akreditasi</p>
           </a>
 
           <a href="http://lims.rsiafamily.com:9494/flims/index.php?p=show_detail&id=25/" target="_blank" rel="noopener noreferrer" className={styles.card}>
-            <h2>Formulir IT SIMRS &rarr;</h2>
+            <h2>FORM IT SIMRS &rarr;</h2>
             <p>
-              Pengajuan akses user baik itu Teramedik, RSCLOUD, Sismadak, ataupun Email
+              Pengajuan akses user baik itu Teramedik, RSCLOUD, Sidokars, ataupun Email
             </p>
           </a>
         </div>
