9e7c3573518f47c4f8242df6c26e750e3eedcd49
Verdi Verdian <41358769+ve-verdian@users.noreply.github.com>
2025-01-22 14:23:34 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,18 +24,8 @@ export default function Home() {
         <div className={styles.grid}>
          <a href="https://tctrain.ekahospital.com/trakcare/train/web/csp/system.Home.cls#/Component/SSUserLogon" target="_blank" rel="noopener noreferrer" className={styles.card}>
             <h2>TrakCare &rarr;</h2>
-            <p>Hospital Information System, Eka Hospital Family</p>
+            <p>Eka Hospital Family</p>
          </a>
-
-          <a href="http://192.168.100.238/nextcloud" target="_blank" rel="noopener noreferrer" className={styles.card}>
-            <h2>RSCLOUD &rarr;</h2>
-            <p>Rumah yang aman untuk semua data Anda, alangkah baiknya di simpan di Cloud</p>
-          </a>
-
-          <a href="http://192.168.100.26" target="_blank" rel="noopener noreferrer" className={styles.card}>
-            <h2>SIDOKARS v1.2 &rarr;</h2>
-            <p>Mengumpulkan, menyimpan, dan mencari dokumen Akreditasi</p>
-          </a>
 
           <a href="http://lims.rsiafamily.com:9494/flims/index.php?p=show_detail&id=25/" target="_blank" rel="noopener noreferrer" className={styles.card}>
             <h2>FORM IT SIMRS &rarr;</h2>
